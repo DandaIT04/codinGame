a0ca9eccfe602cd15fe994d3ec5a8099f4714cc9
DandaIT04 <73043131+DandaIT04@users.noreply.github.com>
2021-12-31 23:22:23 +0800

    accidentally submitted old code

--- 8_codingame.py
+++ 8_codingame.py
@@ -2,19 +2,23 @@ class Solution(object):
     def sortDict(self,n,t):
         d = {}
 
-        countOnce = 0
         for count,value in enumerate(n):
             d[n[count]] = t[count]
 
         d = dict(sorted(d.items()))    
+        e = list(d.values())
+        f = ""
+        
+        for count1,value1 in enumerate(e):
+            f += e[count1]
 
-        print(d)
+        print(f)
 
-n = list(str(15243))
-t = list("enaet")
+#n = list(str(15243))
+#t = list("enaet")
 
-#n = list(str(input()))
-#t = list(input())
+n = list(str(input()))
+t = list(input())
 
 runSolution = Solution()
 
